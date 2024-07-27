db78b8876404e8d04cfd02b232d3085faa1cdfda
biamney ortega <biamneyo5@gmail.com>
2024-07-27 14:31:21 -0600

    Acomodo 2

--- WindowsFormsApp2/inscripcion.Designer.cs
+++ WindowsFormsApp2/inscripcion.Designer.cs
@@ -318,19 +318,19 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(689, 268);
+            this.button1.Location = new System.Drawing.Point(677, 268);
             this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(56, 33);
+            this.button1.Size = new System.Drawing.Size(68, 33);
             this.button1.TabIndex = 12;
             this.button1.Text = "agregar";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(663, 306);
+            this.button2.Location = new System.Drawing.Point(677, 306);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(91, 47);
+            this.button2.Size = new System.Drawing.Size(77, 47);
             this.button2.TabIndex = 13;
             this.button2.Text = "Actualizar";
             this.button2.UseVisualStyleBackColor = true;
